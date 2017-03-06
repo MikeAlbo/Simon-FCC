5795fd35009c0889043a4ca40492fb27605cec08
Michael Albonetti <michaelalbonetti@Michaels-MacBook-Pro.local>
2017-03-05 21:51:45 -0600

    refine layout, begin testing logic

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,1 +1,27 @@
 // game logic
+
+var Logic = function(){
+    var counter = 0,
+        data = [],
+        gameSpeed = 1000,
+        playbackMoveLength = 1000,
+        dangerMode = false,
+        strictMode = false,
+        startGame = false;
+    
+    this.addPiece = function(){
+        var move = Math.ceil(Math.random() * 4);
+        data.push(move); // add data to dataset
+        playbackPiece(gameSpeed, playbackMoveLength);
+        
+    };
+    
+    function playbackPiece(gameSpeed, playbackMoveLength){
+        // gamespeed sets timeout for each piece playback
+        // playbackMoveLength sets time each piece plays
+        // switch statement used to choose which buttons css to modify and which sound to play
+    }
+    
+}
+
+
--- js/userInterface.js
+++ js/userInterface.js
@@ -1,1 +1,16 @@
 // user interface
+
+var boardRotation = 0;
+
+$(document).ready(function(){
+    
+    // danger button demo
+    $("#dangerButton").on("click", function(){
+        
+        boardRotation += 90;
+        
+        $("#groupedGamePieces").css("transform", "rotate("+boardRotation+"deg)");
+        
+        $("#outputScreen p").text(boardRotation);
+    });
+}); // doc ready
