18bbabeb09f69cfbfda25558b083ce285ff253c9
Michael Albonetti <michaelalbonetti@Michaels-MacBook-Pro.local>
2017-03-09 16:07:54 -0600

    begin logic constructor setup

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -4,24 +4,125 @@ var Logic = function(){
     var counter = 0,
         data = [],
         gameSpeed = 1000,
-        playbackMoveLength = 1000,
+        duration = 1000,
         dangerMode = false,
         strictMode = false,
-        startGame = false;
+        startGame = false,
+        userTurn = false;
     
-    this.addPiece = function(){
-        var move = Math.ceil(Math.random() * 4);
-        data.push(move); // add data to dataset
-        playbackPiece(gameSpeed, playbackMoveLength);
-        
-    };
-    
-    function playbackPiece(gameSpeed, playbackMoveLength){
-        // gamespeed sets timeout for each piece playback
-        // playbackMoveLength sets time each piece plays
-        // switch statement used to choose which buttons css to modify and which sound to play
+
+    // generate a new move
+    function newMove(){
+        return Math.ceil(Math.random * 4);
     }
     
-}
+    // timeout for move duration
+    function durationTimeout(e){
+        setTimeout(function(){
+            return e;
+        }, duration);
+    }
+    
+    // timeout for seq speed
+    function seqSpeed(e){
+        setTimeout(function(){
+            return e++;
+        }, gameSpeed);
+    }
+    
+    // select playback piece
+    
+    function selectPiece(piece, type){ // type defines add/ remove
+        switch(piece){
+            case 1 : console.log("blue");
+            case 2 : console.log("red");
+            case 3 : console.log("yellow");
+            case 4 : console.log("green");
+            default : console.log("bad piece");
+        }
+    }
+    
+    //modify user turn
+    function modifyUserTurn(){
+        userTurn = !userTurn;
+    }
+    
+    // handle error input
+    function erroInput(){
+        // playback piece audio (error tone) and flash piece
+      if(strictMode){
+          //reset game
+      }    
+    };
+    
+    function seqComplete(){
+        counter = 0;
+        modifyUserTurn();
+        // playback chime and message
+    }
+    
+    function gameWon(){
+        // game won animation and audio seq
+        // reset game
+    }
+    
+    // add new move to seq
+    this.addMove  = function(){
+        data.push(newMove());
+    };
+    
+    // playback seq
+    
+    this.seqPlayback = function(){
+        var playback = true;
+        var i = 0;
+        while(playback){
+            selectPiece(data[i], "add");
+            durationTimeout(selectPiece(data[i], "remove"));
+            if(i < data.length) {
+                i = seqSpeed(i);   
+            } else {
+                modifyUserTurn();
+                return !playback;
+            }
+        } 
+        return;   
+    };  // seq playback
+    
+    
+    // user seqValidator
+    
+    this.seqValidator = function(move){
+        if(userTurn){
+            if(move == data[counter]){
+               counter++; 
+            } else {
+                handleErrorInput();
+            }
+        }
+        
+        if(counter == 20){
+            return gameWon();
+        }
+        
+        if(counter == data.length){
+            return seqCompelete();
+        }
+    }
+    
+    
+    
+    
+    // strict mode 
+    this.strictMode = function(){
+        strictMode = !strictMode;
+    }
+    
+    // danger mode
+    this.dangerMode = function(){
+        dangerMode = !dangerMode;
+    }
+    
+} // logic constructor
 
 
--- js/userInterface.js
+++ js/userInterface.js
@@ -1,18 +1,68 @@
 // user interface
 
-var boardRotation = 0;
+
 
 $(document).ready(function(){
     
-    // danger button demo
-    $("#dangerButton").on("click", function(){
-        
-        boardRotation += 90;
-        
-        $("#groupedGamePieces").css("transform", "rotate("+boardRotation+"deg)");
-        
-        $("#outputScreen p").text(boardRotation);
-        
+    var boardRotation = 0,
+        newGame = null;
+    
+    // link DOM elements
+    var playButton = $("#playButton");
+    var buttonOne = $("#simonTouchOne");
+    var buttonTwo = $("#simonTouchTwo");
+    var buttonThree = $("#simonTouchThree");
+    var buttonFour = $("#simonTouchFour");
+    
+    var output = $("#outputScreen");
+    var startButton = $("#startResetButton");
+    var strictButton = $("#strictModeButton");
+    var dangerButton = $("#dangerButton");
+    
+    // keyboard listener;
+    
+    $(document).keydown(function(e){
+        switch(e.which){
+            case 37: keyTestFunction("left");
+            break;
+            case 38: keyTestFunction("up");
+            break;
+            case 39: keyTestFunction("right");
+            break;
+            case 40: keyTestFunction("down");
+            break;
+            case 83: keyTestFunction("s")
+            break;
+            case 68: keyTestFunction("d");
+            break;
+            case 88: keyTestFunction("x");
+            break;
+            default: return;
+        }
     });
+    
+    function keyTestFunction(e){
+        console.log("key: " + e);
+    }
+    
+    
+    // strict button
+    
+    $(strictButton).click(function(){
+        if(!newGame){
+            // create a new game
+        }
+        newGame.strictMode();
+    });
+    
+    // danger button 
+    $(dangerButton).click(function(){
+        if(!newGame){
+            // create new game
+        }
+        newGame.dangerMode();
+    })
+    
+    
     
 }); // doc ready
